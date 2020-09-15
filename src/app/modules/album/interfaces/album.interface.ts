18290e6931ea6ad22f64f551f581bcb060f68409
William A. Umaña <waumana@teknei.com>
2020-09-15 17:31:48 +0200

    Album interfaces

---
+++ src/app/modules/album/interfaces/album.interface.ts
@@ -0,0 +1,11 @@
+export interface Album {
+  _id: String;
+  title: String;
+  artistId: String;
+  coverUrl: String;
+  year: number;
+  genre: string;
+  _createdAt: String,
+  _updatedAt: String,
+  __v: Number
+}
