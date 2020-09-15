9a3b238abdf80e03398785c94c5dca74a60b0eb2
William A. Umaña <waumana@teknei.com>
2020-09-15 17:31:17 +0200

    album/delete component

---
+++ src/app/modules/album/components/delete/delete.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+
+export class DeleteComponent implements OnInit {
+
+  //ID del artista a eliminar dado desde el componente padre
+  @Input() idArtist;
+
+  constructor(
+    public activeModal: NgbActiveModal
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * @name closeModal
+   * @description Metodo para cerrar el modal sin confirmar
+  */
+  closeModal(){
+    this.activeModal.close();
+  }
+
+  /**
+   * @name confirmModal
+   * @description Método para confirmar la eliminación del album.
+   * @param id ID del album a eliminar.
+   */
+  confirmModal(id) {
+    this.activeModal.close(id);
+  }
+
+}
