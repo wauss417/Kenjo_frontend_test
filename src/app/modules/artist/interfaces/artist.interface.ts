84c3e575c3fb065f43311244b8e721896157ac50
William A. Umaña <waumana@teknei.com>
2020-09-15 16:47:05 +0200

    artist/ interfaces

---
+++ src/app/modules/artist/interfaces/artist.interface.ts
@@ -0,0 +1,10 @@
+export interface Artist {
+  _id: String;
+  name: String;
+  photoUrl: String;
+  birthdate: string;
+  deathDate: string;
+  _createdAt: String,
+  _updatedAt: String,
+  __v: Number
+}
